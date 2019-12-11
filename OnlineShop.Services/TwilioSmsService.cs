0f88344c5ec743f753d0c346e1898a00fc157e2c
PressFToPayTaxes <igor_2002_25@mail.ru>
2019-12-11 18:25:43 +0600

    Init

---
+++ OnlineShop.Services/TwilioSmsService.cs
@@ -0,0 +1,20 @@
+﻿using OnlineShop.DTO;
+using OnlineShop.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Services
+{
+    public class TwilioSmsService : ISmsService
+    {
+        public Task<SmsServiceResponseDTO> SendVerificationCode(string phoneNumber, string code)
+        {
+            return Task.FromResult(new SmsServiceResponseDTO { 
+                StatusCode = 200,
+                Message = "Сообщение успешно отправлено"
+            });
+        }
+    }
+}
