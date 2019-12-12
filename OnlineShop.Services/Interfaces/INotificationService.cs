d53e1ab23405494886d44cf4410bce3edc180712
PressFToPayTaxes <igor_2002_25@mail.ru>
2019-12-12 18:09:00 +0600

    Added Payment and Firebase

---
+++ OnlineShop.Services/Interfaces/INotificationService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Services.Interfaces
+{
+    public interface INotificationService
+    {
+        Task SendInformMessage(List<string> ids, string message);
+    }
+}
---
+++ OnlineShop.Services/Interfaces/IPaymentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineShop.Domain;
+using OnlineShop.DTO;
+
+namespace OnlineShop.Services.Interfaces
+{
+    public interface IPaymentService
+    {
+        Task<PaymentServiceResponseDTO> CreateInvoice(Order order);
+    }
+}
